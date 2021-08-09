f4fdd5910f6e01bf77b05db44e232e913651da63
Antti Turunen <asturune@hotmail.com>
2021-08-09 11:52:50 +0300

    6-kirjan lisäys ja authorin syntymävuoden muokkaus lisätty

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 
 const { ApolloServer, gql } = require('apollo-server')
+//Mutaatioita varten ID-generaattori, kun luodaan uusia objekteja
+const { v1: uuid } = require('uuid')
+
 
 let authors = [
     {
@@ -86,7 +89,7 @@ let books = [
         genres: ['classic', 'revolution']
     },
 ]
-
+//"const typeDefs = gql`" --> sisältää sovelluksen käyttämän GraphQL-skeeman
 const typeDefs = gql`
 type Author {
     name: String!
@@ -108,21 +111,59 @@ type Author {
       bookCount: Int!
       allBooks(author: String ,genre: String): [Book!]!
       allAuthors: [Author!]!
+      simplyAllBooks:[Book!]!
 }
-`
+type Mutation{
+    addBook(
+        title: String!
+        published: Int!
+        author: String!
+        genres: [String]        
+    ): Book
+    editAuthor(
+        name: String!
+        setBornTo: Int!
+      ): Author
+}
 
+`
+//tsekataan, että tulostuuko cmd:hen kirjat halutulla ehdolla
 console.log('ALL BOOKS BY Author', books.filter(b => b.genres.find(g => g === 'refactoring')))
+console.log('ALL BOOKS', books)
+
+const bookYksi = {
+    title: 'titteli',
+    published: 2008,
+    author: 'Atu Atunen',
+    id: "afa5b6f4-344d-11e9-a414-719c6709cf3e",
+    genres: ['refactoring']
+
+}
+
+console.log('ADD BOOK', books.concat(bookYksi))
+
+//"const resolvers = {" --> määrittelee miten GraphQL-kyselyihin vastataan
 const resolvers = {
     Query: {
+        //Ks. const typeDefs = gql` ja sieltä type Query
         authorCount: () => authors.length,
         bookCount: () => books.length,
+        simplyAllBooks: () => books,
+        /*Haetaan kaikki krijat-->kirjoita tämä graphql selaimeen ja paina "play"-nappia
+        query {
+            simplyAllBooks{ 
+                title
+            }
+        }
+        */
         //Argumenttia hyödynnetään eli http://localhost:4000/graphql käyttöliittymässä
         //voidaan hakea halutulla authorin nimellä authorin kaikki kirjat. 
         //ks.yllä " allBooks(author: String!): [Book!]!"
         //HUOM! Haku tehdään näin http://localhost:4000/graphql käyttöliittymässä
         /*
+        Kirjoita tämä graphql käyttöliittymään:
         query {
-            allBooks(author: "Robert Martin") {
+            allBooks(author: "Martin Fowler") {
             title
             }
         }
@@ -151,6 +192,53 @@ const resolvers = {
                     .length)
 
 
+        }
+    }
+    ,
+    Mutation: {
+        addBook: (root, args) => {
+            //Jos kirjailijaa ei ole, niin lisätään authors -taulukkoon
+            if (!authors.find(a => a.name === args.author)) {
+                const author = {
+                    name: args.author,
+                    id: uuid(),
+                    born: 1
+                }
+                authors = authors.concat(author)
+                console.log('MENIKÖ NIMI SISÄÄN', args.author)
+                console.log('MENIKÖ NIMI SISÄÄN', author)
+            }
+
+            const book = { ...args, id: uuid() }
+            books = books.concat(book)
+            console.log('Kirja', book)
+            return book
+        }
+        /* Kirja lisätään näin, kirjoittamalla tämä komento graphQl konsoliin
+        mutation {
+            addBook(
+                title: "Pimeyden tango",
+                author: "Reijo Mäki",
+                published: 1997,
+                genres: ["crime"]
+            ) {
+                title,
+                author
+            }
+    }*/
+        ,
+        editAuthor: (root, args) => {
+            console.log('Tuliko editAuthoriin', args.name)
+            const author = authors.find(a => a.name === args.name)
+            if (!author) {
+                return null
+            }
+            console.log('Lähtikö muuttamaan editAuthorissa setBornTo arvolla', args.setBornTo)
+            //HUOM! Skeemassa pitää olla "setBornTo", koska konsolesta syötetään sen nimisellä parametrilla
+            //uusi syntymävuosi
+            const updatedAuthor = { ...author, born: args.setBornTo }
+            authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+            return updatedAuthor
         }
     }
 }
